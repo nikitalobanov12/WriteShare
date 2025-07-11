b078c54267f011e6b4c4589d4bcd5418235239f7
nikitalobanov12 <nikitaalobanovv@gmail.com>
2025-07-10 19:03:37 -0700

    refactor

--- src/lib/dal.ts
+++ src/lib/dal.ts
@@ -1,11 +1,11 @@
 import { SessionService } from "./session-service";
 import { UserRepository } from "./user-repository";
 
-// Instantiate services
+// main logic lives here.
 export const sessionService = new SessionService();
 export const userRepository = new UserRepository();
 
-// Optionally, provide backward-compatible exports for existing consumers
+// provide backwards compatible exports for previous implementations in my app
 export const verifySession = sessionService.verifySession;
 export const checkUserPermission = sessionService.checkUserPermission;
 export const getUser = async () => {
